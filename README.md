08cf725cec349a02d349b7cec5fa7ed44ea11e66
mushfiqur-rahman <mushfiq.style@gmail.com>
2022-05-09 12:41:04 +0600

    project completed

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # DjangoRestAPI
+
+****
+**This is my first python project. I tried to make two main parts of this project. One BacEndPart that made by django and another FrontEnd part that made by Vuejs v-2. Both project connected through AXIOS API**
+
+### Customize Python Installation on your Machine
+See [MyVideo](https://www.youtube.com/watch?v=NYnYvP226LE).
+
+## Project setup of djangobackend
+
+### Create Virtual Environment for this project
+```
+py -m venv venv
+```
+### Virtualization Activation
+```
+venv\Scripts\Activate
+```
+### Install all required Packages
+```
+pip install -r requirements.text
+```
+### To database migration
+```
+py manage.py migrate
+```
+
+```
+py manage.py runserver
+```
+
---
+++ djangobackend/apiapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.permissions import IsAuthenticated
+
+# Create your views here.
+# RetrieveAPIView - used for read-only endpoints to represent a single model instance.
+# https://www.django-rest-framework.org/api-guide/generic-views/#retrieveapiview
+class PostView(generics.RetrieveAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = Post.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = PostSerializer(queryset, many=True)
+        return Response(serializer.data)
